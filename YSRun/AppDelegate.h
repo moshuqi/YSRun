42a2608650b7cb5f470d440183f4ffc66e4388a1
moshuqi <270304102@qq.com>
2015-10-15 11:02:06 +0800

    Initial Commit

---
+++ YSRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YSRun
+//
+//  Created by moshuqi on 15/10/15.
+//  Copyright © 2015年 msq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YSRun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSRun
+//
+//  Created by moshuqi on 15/10/15.
+//  Copyright © 2015年 msq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
